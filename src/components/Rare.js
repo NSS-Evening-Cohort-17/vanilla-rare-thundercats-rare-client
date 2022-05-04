d28a4fbd2fff04d333391f0c31f349a586504efb
Jake Chestnut <jacobschestnut@gmail.com>
2022-05-03 20:30:56 -0500

    added post related modules

--- src/components/Rare.js
+++ src/components/Rare.js
@@ -4,7 +4,7 @@ import { ApplicationViews } from "./ApplicationViews"
 import { NavBar } from "./nav/NavBar"
 import { Login } from "./auth/Login"
 import { Register } from "./auth/Register"
-
+import { Posts } from "./posts/Posts"
 export const Rare = () => {
   const [token, setTokenState] = useState(localStorage.getItem('token'))
 
@@ -33,6 +33,11 @@ export const Rare = () => {
     <Route path="/register" exact>
       <NavBar token={token} setToken={setToken} />
       <Register token={token} setToken={setToken} />
+    </Route>
+
+    <Route path ="/" exact>
+      <NavBar token={token} setToken={setToken} />
+      <Posts token={token} setToken={setToken} />
     </Route>
 
   </>
