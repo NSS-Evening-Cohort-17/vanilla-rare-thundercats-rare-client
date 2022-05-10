c51f37b05fbdcf6011f12ea4125cda331cf1d8c6
root <root@LAPTOP-HPHIU1FQ.localdomain>
2022-05-09 19:11:20 -0500

    added users component also created a fetch call to retrieve data from the database

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,8 +1,13 @@
 import React from "react"
 import { Route } from "react-router-dom"
+import { UserCard } from "./user/UserCard"
+import { UserList } from "./user/UserList"
 
-export const ApplicationViews = () => {
+export const ApplicationViews = ({token, setToken}) => {
   return (
+    <>
+    <UserList></UserList>
     <h1 >Welcome to Rare Publishing</h1>
+    </>
   )
 }
---
+++ src/components/user/UserList.js
@@ -0,0 +1,21 @@
+import React, {useState, useEffect} from 'react'
+import { getUsers } from './UserManager'
+import { UserCard } from './UserCard';
+
+export const UserList = () => {
+    const [users, setUsers] = useState(localStorage.getItem('token'));
+
+    useEffect(() => {
+        getUsers().then(theUsers => {
+            setUsers(theUsers);
+        });
+    }, [])
+
+    console.log(users)
+
+    return (
+        <div className='container'>
+            {users.map((user) => <UserCard key={user.id} user={user}/>)}
+        </div>
+    )
+}
